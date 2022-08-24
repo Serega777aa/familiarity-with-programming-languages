33ace363bf03c705882be46a9f27dd857241e92e
Your Name <serega777aa@mail.ru>
2022-08-24 17:18:59 +0300

    add HomeWork1

---
+++ HomeWork1/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}    
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
---
+++ HomeWork1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+Console.WriteLine("max = " + max);
+
