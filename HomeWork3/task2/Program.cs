5b777ca85eddf3e6a6bc570d3aec3f7f3bf71215
Your Name <serega777aa@mail.ru>
2022-08-26 16:06:42 +0300

    add task2

---
+++ HomeWork3/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координаты A");
+int xA = Convert.ToInt32(Console.ReadLine());
+int yA = Convert.ToInt32(Console.ReadLine());
+int zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты B");
+int xB = Convert.ToInt32(Console.ReadLine());
+int yB = Convert.ToInt32(Console.ReadLine());
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double distAB = Math.Round((Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2))), 2);
+Console.WriteLine($"расстояние между точками A и B  {distAB}");
