bbcdf18b633d7de90ba1935fc7eca52277e74938
Your Name <serega777aa@mail.ru>
2022-08-26 14:10:01 +0300

    add task1

---
+++ HomeWork3/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int pal = 0;
+
+while (temp > 0)
+{
+    pal = pal * 10 + temp % 10;
+    temp /= 10;
+}
+if (num == pal)
+{
+    Console.WriteLine("Палиндром");
+}
+else 
+{
+     Console.WriteLine("Не является палиндромом");
+}
+
+
