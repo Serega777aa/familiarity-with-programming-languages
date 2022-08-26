fd0e2836783fdc254976cff36fee5d835f36a7f4
Your Name <serega777aa@mail.ru>
2022-08-26 17:55:03 +0300

    add task3

---
+++ HomeWork3/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int [N];
+int num = 1;
+
+
+for (int i = 0; i < N; i++)
+{
+    arr[i] = num * num * num;
+    Console.Write(arr[i] + " ");
+    num++;
+}
+
