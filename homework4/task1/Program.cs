5b6d055e1ff3d6f88543dd67034dfb93803ab73f
Your Name <serega777aa@mail.ru>
2022-08-30 14:39:26 +0300

    add task1

---
+++ homework4/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int expon = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < expon; i++)
+{
+    result *= num;
+}
+
+Console.WriteLine(result);
