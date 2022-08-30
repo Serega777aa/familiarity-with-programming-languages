0e5749ed5253039bb6bb846566f00e148eecdb2b
Your Name <serega777aa@mail.ru>
2022-08-30 15:12:25 +0300

    add task2

---
+++ homework4/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = 0;
+int tempNum = num;
+int tempDigit = 0;
+
+while (tempNum > 0)
+{
+    tempDigit =  tempNum % 10;
+    if (tempDigit != 0)
+    {
+        if (num % tempDigit == 0)
+        {
+            sumDigit += tempDigit;
+        }
+    }
+    tempNum /= 10;
+}
+Console.WriteLine(sumDigit);
