58534b214e9f43c0df0a48ceab0edbd4e603a036
Your Name <serega777aa@mail.ru>
2022-08-30 17:42:07 +0300

    add task3

---
+++ homework4/task3/Program.cs
@@ -0,0 +1,57 @@
+﻿int MultiplDigits(int number)
+{
+    int multipl = 1;
+    int temp = number;
+    while (number > 0)
+    {
+        temp = number % 10;
+        if (temp != 0)
+        {
+            multipl *= temp;
+            number /= 10;
+        }
+        else
+        {
+            number /= 10;
+        }
+    }
+    return multipl;
+}
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    int temp = number;
+    while (number > 0)
+    {
+        temp = number % 10;
+        sum += temp;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] interesNums = new int[n];
+
+int randNum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    
+    while (interesNums[i] == 0)
+    {
+        randNum =  new Random().Next();
+        if (MultiplDigits(randNum) % SumDigits(randNum) == 0)
+        {
+            interesNums[i] = randNum;
+        }
+    }
+}
+for (int index = 0; index < n; index++)
+{
+    Console.Write($"{interesNums[index]} ");
+}
+
