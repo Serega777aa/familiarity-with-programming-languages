2b5bd2388670b28749861a7dddf94476be4f596e
Your Name <serega777aa@mail.ru>
2022-09-02 16:17:42 +0300

    add task2 in homework5

---
+++ homework5/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+Random rand = new Random();
+int sumEven = 0;
+
+for (int i = 0; i < n; i++)
+{
+    numbers[i] = rand.Next(10, 100);
+    if (i % 2 != 0)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            sumEven += numbers[i];
+        }
+    }
+}
+PrintArray(numbers);
+Console.WriteLine($"сумма четных елементов, стоящих на нечетных позициях равна {sumEven}");
