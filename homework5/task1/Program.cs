1152f0c9c7c77a66c784474c54c69d38a75d5ad1
Your Name <serega777aa@mail.ru>
2022-09-02 15:54:43 +0300

    add task1 in homework5

---
+++ homework5/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+Random rand = new Random();
+int countEven = 0;
+
+for (int i = 0; i < n; i++)
+{
+    numbers[i] = rand.Next(100, 1000);
+    if (numbers[i] % 2 == 0)
+    {
+        countEven++;
+    }
+}
+PrintArray(numbers);
+Console.WriteLine($"в массиве {countEven} четных елемента");
