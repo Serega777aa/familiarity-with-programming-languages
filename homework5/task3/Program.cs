c2dcd1cb769b56e9386ad050959a202a2a6bc11f
Your Name <serega777aa@mail.ru>
2022-09-02 17:02:52 +0300

    add task3 in homework5

---
+++ homework5/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+Random rand = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    numbers[i] = rand.Next(1, 100);
+}
+int minNumber = numbers[0];
+int maxNumber = numbers[0];
+
+for (int i = 1; i < n; i++)
+{
+    if (numbers[i] < minNumber)
+    {
+        minNumber = numbers[i];
+    }
+    if (numbers[i] > maxNumber)
+    {
+        maxNumber = numbers[i];
+    }
+}
+int diffMaxMin = maxNumber - minNumber;
+PrintArray(numbers);
+Console.WriteLine("Разница между максимальным и минимальным элементов массива = " + diffMaxMin);
