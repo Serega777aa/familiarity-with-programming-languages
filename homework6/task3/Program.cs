64b45ea076de8b651a8f8e4b6a98e7c94acf1cc9
Your Name <serega777aa@mail.ru>
2022-09-06 16:28:34 +0300

    add task3 in homework6

---
+++ homework6/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("ВВедите размер массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+{
+    arr[i] = rand.Next(1, 21);
+}
+int sum = 0;
+for (int i = 0; i < n / 2; i++)
+{
+    sum += arr[i] * arr[n - 1 - i];
+}
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+
+Console.WriteLine($"-> {sum}");
+
