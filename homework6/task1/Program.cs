7c4b26f786a929026c5ce1360a6f978b4bc53342
Your Name <serega777aa@mail.ru>
2022-09-06 14:12:39 +0300

    add task1 in homework6

---
+++ homework6/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("ВВедите натуральное число ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[m];
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0)
+        count++;
+}
+for (int i = 0; i < m; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+ Console.Write($"-> {count} ");
+
+    
