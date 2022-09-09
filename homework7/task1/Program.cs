30d5f7df3ed22343e7c03da7eaaf921c65606e34
Your Name <serega777aa@mail.ru>
2022-09-09 11:36:53 +0300

    add task1 in homework7

---
+++ homework7/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество строк массива ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Math.Round(rand.NextDouble() * 20 - 10, 1);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
