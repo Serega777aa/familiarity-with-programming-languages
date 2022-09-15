77074f7ca4c6bafcf15e326f3fe1b5119990268e
Your Name <serega777aa@mail.ru>
2022-09-15 16:50:15 +0300

    add task3 in homework8

---
+++ homework8/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿bool Contains (int[,] arr, int value)
+{
+    bool flag = false;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == value)
+            {
+                flag = true;
+                break;
+            }
+        }
+    }
+    return flag;
+}
+
+
+int [,] arr = new int [3, 3];
+Random rand = new Random();
+int temp = 0;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        bool flag = true;
+        while (flag == true)
+        {
+            temp = rand.Next(10, 100);
+            flag = Contains(arr, temp);
+        }
+        arr[i, j] = temp;
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
