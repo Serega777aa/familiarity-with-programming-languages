35324b53c03978a2a6d628748f6da81586d5ece2
Your Name <serega777aa@mail.ru>
2022-09-18 16:35:44 +0300

    add task3 in homework9

---
+++ homework9/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+int ak = AkkermanFunction(2, 2);
+Console.WriteLine(ak);
