12896f2adc7c1ee7d1a1030e76fff74dd8539fb7
Your Name <serega777aa@mail.ru>
2022-09-18 11:22:30 +0300

    add task1 in homework9

---
+++ homework9/task1/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortLinesArray(int[,] array)
+{
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+
+                }
+            }
+        }
+    }
+}
+
+int[,] arr = CreateArray(3, 4);
+PrintArray(arr);
+Console.WriteLine();
+SortLinesArray(arr);
+PrintArray(arr);
+
+
